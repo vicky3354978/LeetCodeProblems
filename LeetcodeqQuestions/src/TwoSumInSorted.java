66b6fcf3523cab2bc779ebe17ec64bcec706b196
vicky kumar <vk3354978@gmail.com>
2026-09-23 01:39:05 +0530

    leetcode 167

---
+++ LeetcodeqQuestions/src/TwoSumInSorted.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int i=0;
+        int j=numbers.length-1;
+        while(i<j)
+        {
+            int sum;
+            sum=numbers[i]+numbers[j];
+            if(sum==target)
+            {
+                return new int[]{i+1,j+1};
+            }
+            if(sum>target)
+            {
+                j--;
+            }
+            else if(sum<target)
+            {
+                i++;
+            }
+        }
+        return new int[]{i+1,j+1};
+
+    }
+}
