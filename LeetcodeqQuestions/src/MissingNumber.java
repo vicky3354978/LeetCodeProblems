7c4bb6ed9214f1db191410ec13c1a507afd1bfae
vicky kumar <vk3354978@gmail.com>
2026-09-23 03:29:31 +0530

    missing number

---
+++ LeetcodeqQuestions/src/MissingNumber.java
@@ -0,0 +1,15 @@
+public class MissingNumber {
+        public int missingNumber(int[] nums) {
+            int n=nums.length;
+            int sum=(n*(n+1))/2;
+            int asum=0;
+            for(int i=0; i<nums.length; i++)
+            {
+                asum=asum+nums[i];
+            }
+            int result;
+            result=sum-asum;
+            return result;
+        }
+    }
+
