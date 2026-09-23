dd45abe0550905ed9602321bf6d03be493381c96
vicky kumar <vk3354978@gmail.com>
2026-09-23 13:15:40 +0530

    shift all 0 into left side of array and 1s into right side

---
+++ LeetcodeqQuestions/src/Arrange0LeftAnd1Right.java
@@ -0,0 +1,27 @@
+public class Arrange0LeftAnd1Right {
+        void segregate0and1(int[] arr) {
+            int i=0;
+            int j=arr.length-1;
+            int temp;
+            while(i<j)
+            {
+                if(arr[i]==0)
+                {
+                    i++;
+                }
+                else if(arr[j]==1)
+                {
+                    j--;
+                }
+                else
+                {
+                    temp=arr[i];
+                    arr[i]=arr[j];
+                    arr[j]=temp;
+                    i++;
+                    j--;
+                }
+            }
+
+        }
+    }
